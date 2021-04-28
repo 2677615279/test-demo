f191d27213e1c9e7d01dcc3e3f9bda3a686f01a7
2677615279 <2677615279@qq.com>
2021-04-28 14:06:13 +0800

    加回src/Demo3.java

---
+++ src/Demo3.java
@@ -0,0 +1,13 @@
+/**
+ * @author: Qu Fan
+ * @date: 2021/4/28
+ * @description: git命令行删除文件
+ */
+public class Demo3 {
+
+    public static void main(String[] args) {
+        System.out.println("这是第二个添加的文件");
+        System.out.println("欢迎大家一起来学习git");
+    }
+
+}
