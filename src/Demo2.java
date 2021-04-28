cb862babead29dc3772aec1a19497a3c56ab9b4a
2677615279 <2677615279@qq.com>
2021-04-28 14:36:58 +0800

    手动添加文件

---
+++ src/Demo2.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Qu Fan
+ * @date: 2021/4/28
+ * @description: 测试git
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println("欢迎大家一起来学习git");
+    }
+
+}
