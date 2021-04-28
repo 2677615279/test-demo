b42dc76a3ff0f1463fcd4f72caa3c6bf64fe4266
2677615279 <2677615279@qq.com>
2021-04-28 15:06:18 +0800

    手动添加文件

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Qu Fan
+ * @date: 2021/4/28
+ * @description: git命令移动文件
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
