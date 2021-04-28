16a775cbd090e51f2ffad74245625a89abe81b17
2677615279 <2677615279@qq.com>
2021-04-28 14:54:39 +0800

    命令行重命名文件

--- src/Home.java
+++ src/Home.java
@@ -3,7 +3,7 @@
  * @date: 2021/4/28
  * @description: 测试git
  */
-public class Demo2 {
+public class Home {
 
     public static void main(String[] args) {
         System.out.println("欢迎大家一起来学习git");
