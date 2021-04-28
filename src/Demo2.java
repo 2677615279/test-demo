f9a13fc4dd6bbcad5aa7f40e2ac84b6f9cf95269
2677615279 <2677615279@qq.com>
2021-04-28 14:30:19 +0800

    手动添加文件

---
+++ src/Demo2.java
@@ -0,0 +1,13 @@
+/**
+ * @author: Qu Fan
+ * @date: 2021/4/27
+ * @description: 测试git
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println("这是第一个添加的文件");
+        System.out.println("欢迎大家一起来学习git");
+    }
+
+}
