2af0aac2c595b8d862f932637f3e57a65b4a91f9
2677615279 <2677615279@qq.com>
2021-04-29 14:12:52 +0800

    版本一

---
+++ src/Version.java
@@ -0,0 +1,12 @@
+/**
+ * @author: Qu Fan
+ * @date: 2021/4/29
+ * @description: 测试git版本回退
+ */
+public class Version {
+
+    public static void main(String[] args) {
+        System.out.println("版本1");
+    }
+
+}
